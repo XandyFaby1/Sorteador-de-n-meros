11d25b90b9d82e18707a7081ded612eb54e25894
XandyFaby1 <xandyfaby1@gmail.com>
2024-12-21 20:07:30 -0300

    projeto inicial

---
+++ app.js
@@ -0,0 +1,18 @@
+function sortear(){
+    let quantidade = parseInt(document.getElementById("quantidade").value);
+    let de = parseInt(document.getElementById("de").value);
+    let ate = parseInt(document.getElementById("ate").value);
+
+    let sorteados = [];
+    let numero;
+
+    for (let i = 0; i < quantidade; i++) {
+        numero = obiterNumeroAliatorio(de, ate);
+        sorteados.push(numero);
+    }
+    alert(sorteados);
+}
+
+function obiterNumeroAliatorio(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
