12f89dfdc633e05873cb891fd2ada61d17290aff
XandyFaby1 <xandyfaby1@gmail.com>
2024-12-23 14:52:58 -0300

    mudancas finais

--- app.js
+++ app.js
@@ -8,12 +8,39 @@ function sortear(){
 
     for (let i = 0; i < quantidade; i++) {
         numero = obiterNumeroAliatorio(de, ate);
+
+        while (sorteados.includes(numero)) {
+            numero = obiterNumeroAliatorio(de, ate);
+        }
+
         sorteados.push(numero);
     }
-    alert(sorteados);
-
+    
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`;
+    alertarStatusDoBotao();
 }
 
 function obiterNumeroAliatorio(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function alertarStatusDoBotao() {
+    let botao = document.getElementById("btn-reiniciar");
+
+    if (botao.classList.contains("container__botao-desabilitado")) {
+        botao.classList.remove("container__botao-desabilitado");
+        botao.classList.add("container__botao");
+    } else {
+        botao.classList.remove("container__botao");
+        botao.classList.add("container__botao-desabilitado");
+    }
+}
+
+function reiniciar() {
+    document.getElementById("quantidade").value = "";
+    document.getElementById("de").value = "";
+    document.getElementById("ate").value = "";
+    document.getElementById("resultado").innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alertarStatusDoBotao();
 }
